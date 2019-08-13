cc74c52e4f469e2fe9f184d8d910ba907f43d439
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 02:38:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   array =   string.split(" ")
-  reverseArray = array.collect {|el| el.reverse}
-  reverseArray.join(" ")
+  array_of_reverse = array.collect {|el| el.reverse}
+  array_of_reverse.join(" ")
 end
